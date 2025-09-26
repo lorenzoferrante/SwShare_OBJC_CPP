9c4be59697b9bf1d59fa83dd7d3429126e45404a
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2025-09-26 21:13:49 +0200

    Modificata la struct get_set_time_answer_t e il metodo prepare_data_for_get_set_time per segnalare lo stato del TROPPOPIENO

--- BLELib2/FunShare.h
+++ BLELib2/FunShare.h
@@ -11,6 +11,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#if defined(__IS_CLIENT__)
+#include <stdbool.h>
+#endif
+
 #define MAX_NUM_READ_WRITE (128)
 #define MAX_MTU (128)
 
@@ -28,13 +32,16 @@ void printFunc(void);
 uint16_t crc16(uint32_t len, uint8_t *buf);
 int32_t prepare_data_for_raw_read (api_query_t *query, api_answer_t *answer, uint32_t addr_device, uint16_t quanti, bool q);
 int32_t prepare_data_for_raw_write (api_query_t *query, api_answer_t *answer, uint8_t * data, uint32_t addr_device, uint16_t quanti, bool q);
-int32_t prepare_data_for_get_set_param (api_query_t *query, api_answer_t *answer, uint16_t answ_size, uint16_t param_num, uint32_t value, uint8_t write, uint8_t q);
+int32_t prepare_data_for_get_set_param (api_query_t *query, api_answer_t *answer, uint16_t param_num, uint32_t value, uint8_t write, uint8_t q);
 int32_t prepare_data_for_gen_command (api_query_t *query, api_answer_t *answer, uint16_t size_query, uint16_t size_answer, uint8_t command, uint8_t q);
+int32_t prepare_data_for_devices_onboard (api_query_t *query, api_answer_t *answer, uint8_t device, uint8_t attivazione, uint8_t q);
+int32_t prepare_data_for_get_set_time (api_query_t *query, api_answer_t *answer, uint32_t * time, uint32_t *cnt_attiv, uint32_t *time_attiv, char *nome, uint32_t *device_overfull, uint8_t get_set, uint8_t q);
 uint8_t isleap(uint8_t year);
 void gettime(uint32_t t, t_calendar *r);
 uint32_t _mktime(t_calendar * t);
 uint32_t maketime(t_calendar *t);
 uint32_t get_my_date(void);
+char* test_func(void);
     
 #ifdef __OBJC__
 @end
